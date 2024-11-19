7a148286af48db070e0a24ba13e32cbc3290c865
ederwinnicius <eder-winnicius@hotmail.com>
2024-11-18 20:44:36 -0800

    add documentation components for Swagger

--- APIVisionary/Program.cs
+++ APIVisionary/Program.cs
@@ -3,16 +3,13 @@ using APIVisionary.Services.Conteudo;
 using APIVisionary.Services.Playlist;
 using APIVisionary.Services.Usuario;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<UsuarioInterface, UsuarioRepository>();
 
@@ -31,6 +28,39 @@ builder.Services.AddControllers()
     {
         options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
     });
+
+
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Visionary API",
+        Description = "System Database MEnegement",
+        TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "Eder Oliveira",
+            Url = new Uri("https://linkedin.com/in/eder-winnicius/")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Example License",
+            Url = new Uri("https://example.com/license")
+        }
+    });
+
+    // Obtendo o caminho do arquivo XML
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
+
+
+
+
 
 var app = builder.Build();
 
